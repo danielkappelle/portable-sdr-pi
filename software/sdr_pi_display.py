a76a3c24267befded392fdfe8f1eda4cd2501417
Daniel Kappelle <daniel.kappelle@gmail.com>
2022-03-07 00:03:07 +0100

    Horrible typo

--- software/sdr_pi_display.py
+++ software/sdr_pi_display.py
@@ -8,14 +8,14 @@ import os
 class SdrPiDisplay:
   def __init__(self, display_width = 128, display_height = 64, addr = 0x3c):
     self.display_width = display_width
-    self.dipslay_height = display_height
+    self.display_height = display_height
     self.addr = addr
     
     # Set up I2C
     self.i2c = busio.I2C(board.SCL, board.SDA)
 
     # Set up oled screen
-    self.oled = adafruit_ssd1306.SSD1306_I2C(self.display_width, self.dipslay_height, self.i2c, addr=self.addr)
+    self.oled = adafruit_ssd1306.SSD1306_I2C(self.display_width, self.display_height, self.i2c, addr=self.addr)
 
     # Maybe do a splash screen later?
     self.oled.fill(0)
@@ -30,22 +30,22 @@ class SdrPiDisplay:
     # First update the mode
     mode = config.get_mode().display_name
     (font_width, font_height) = self.font.getsize(mode)
-    self.draw.rectangle((0, 0, self.display_width//2, self.dipslay_height), fill=0) # Erase area
-    self.draw.text((6, self.dipslay_height//2-font_height//2), mode, font=self.font, fill=255)
+    self.draw.rectangle((0, 0, self.display_width//2, self.display_height), fill=0) # Erase area
+    self.draw.text((6, self.display_height//2-font_height//2), mode, font=self.font, fill=255)
 
     # Update channel/frequency
     channel = config.get_mode().get_channel_display()
     (font_width, font_height) = self.font.getsize(channel)
-    self.draw.rectangle((self.display_width//2, 0, self.display_width, self.dipslay_height), fill=0)
-    self.draw.text((70, self.dipslay_height//2-font_height//2), channel, font=self.font, fill=255)
+    self.draw.rectangle((self.display_width//2, 0, self.display_width, self.display_height), fill=0)
+    self.draw.text((70, self.display_height//2-font_height//2), channel, font=self.font, fill=255)
 
     # Update changing box
     if config.editing == "channel":
-      self.draw.rectangle((self.display_width//2+4, 4, self.display_width-4, self.dipslay_height-4), fill=None, outline=255)
-      self.draw.rectangle((4, 4, self.display_width//2-4, self.dipslay_height-4), fill=None, outline=0)
+      self.draw.rectangle((self.display_width//2+4, 4, self.display_width-4, self.display_height-4), fill=None, outline=255)
+      self.draw.rectangle((4, 4, self.display_width//2-4, self.display_height-4), fill=None, outline=0)
     else:
-      self.draw.rectangle((self.display_width//2+4, 4, self.display_width-4, self.dipslay_height-4), fill=None, outline=0)
-      self.draw.rectangle((4, 4, self.display_width//2-4, self.dipslay_height-4), fill=None, outline=255)
+      self.draw.rectangle((self.display_width//2+4, 4, self.display_width-4, self.display_height-4), fill=None, outline=0)
+      self.draw.rectangle((4, 4, self.display_width//2-4, self.display_height-4), fill=None, outline=255)
 
     self.oled.image(self.image)
     self.oled.show()
